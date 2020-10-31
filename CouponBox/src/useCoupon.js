491c60079407c88aa2818e8abb654770f77ec87e
qr96 <qr96@naver.com>
2020-10-31 19:48:35 +0900

    쿠폰 슬라이드 효과 추가
    쿠폰함 슬라이드 효과 추가

--- CouponBox/src/useCoupon.js
+++ CouponBox/src/useCoupon.js
@@ -6,11 +6,20 @@
  * @flow
  */
 import 'react-native-gesture-handler';
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { Component,useState } from 'react';
+import { View, Text, Button, Dimensions, ScrollView, StatusBar } from 'react-native';
+
+
 
 class UseCouponScreen extends Component{
   render () {
+    const stamps = [
+      { 'CafeName': "default1", 'number' : 4},
+      { 'CafeName': "default2", 'number' : 6},
+      { 'CafeName': "default3", 'number' : 3},
+      { 'CafeName': "default4", 'number' : 8},
+      { 'CafeName': "default5", 'number' : 9},
+  ]
     const {params} = this.props.route;
     const db = params ? params.db : null;
     const handleClick = () => {
@@ -30,31 +39,41 @@ class UseCouponScreen extends Component{
         }
       });
     };
-    const handleClick2 = () => {
-      db.collection('test')
-      .doc('User')
-      .collection('UserList')
-      .doc('User1')
-      .collection('Stamps')
-      .get()
-      .then( allStamp => {
-        allStamp.forEach(doc => {
-          alert('카페 이름:' + doc.data().CafeName + '\n도장개수:'+doc.data().StampNumber);
-        });
-      })
-    }
+
+    const { width, height } = Dimensions.get('window');
+
     return (
-      <View style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center'
-      }}>
-        <Text>Use Coupon</Text>
-        <Button
-            title="개수 조회"
-            onPress={()=>{ handleClick2() }}
-        />
-      </View>
+      
+      <ScrollView
+        style={{ flex: 1 }}
+        horizontal={true}
+        scrollEventThrottle={16}
+        pagingEnabled={true}
+      >
+        <View style={{ width, height }}>
+          <Text>카페 이름 : {stamps[0].CafeName}</Text>
+          <Text>쿠폰 갯수 : {stamps[0].number}</Text>
+          <Button
+            title="새로고침"
+            onPress={()=>{handleClick()}}
+          />
+          
+        </View>
+        <View style={{ width, height }}>
+        <Text>카페 이름 : {stamps[1].CafeName}</Text>
+        <Text>쿠폰 갯수 : {stamps[1].number}</Text>
+        </View>
+        <View style={{ width, height }}>
+          <Text>Screen 3</Text>
+        </View>
+        <View style={{ width, height }}>
+          <Text>Screen 4</Text>
+        </View>
+        <View style={{width, height }}>
+          <Text>Use Coupon</Text>
+        </View>
+      </ScrollView>
+      
     )
   }
 }
